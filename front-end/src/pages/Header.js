f65220b61ef67c56f6c5c41db94365c621db5ced
eden zewdu <edenzewdu434@gmail.com>
2023-10-22 07:02:45 +0300

    lord-icon cart added

--- front-end/src/pages/Header.js
+++ front-end/src/pages/Header.js
@@ -9,9 +9,12 @@ const Header = () => {
   return (
     <div>
       <div>
-        <lord-icon
-          src={require('../Images/Lordicon/wired-flat-146-trolley.json')}
-        ></lord-icon>
+      <lord-icon
+    src="https://cdn.lordicon.com/cosvjkbu.json"
+    trigger="hover"
+    colors="primary:#c7166f,secondary:#911051,tertiary:#5c0a33"
+    style={{width:"250px", height:'250px'}}>
+</lord-icon>
       </div>
       <div>
         <img src={require('../Images/AB_ETHIOPIAN_TRADITIONAL_CLOTHES__2_-removebg-preview.png')} alt='logo' />
