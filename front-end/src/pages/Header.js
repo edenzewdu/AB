366a121c1598dbc6c119d7512a8f99f9cd0aa991
eden zewdu <edenzewdu434@gmail.com>
2023-10-22 05:54:29 +0300

    creating homepage

--- front-end/src/pages/Header.js
+++ front-end/src/pages/Header.js
@@ -1,34 +1,16 @@
-import { StyleSheet, View } from 'react-native';
-import { useEffect, useRef } from 'react';
-import { Player } from '@lordicon/react';
+import lottie from "lottie-web";
+import { defineElement } from "@lordicon/element";
 
-const ICON = require('../Images/Lordicon/wired-flat-146-trolley (1).json');
-
-export default function App() {    
-  const playerRef = useRef(null);
-  
-  useEffect(() => {
-      playerRef.current?.playFromBeginning();
-  }, [])
-
+// define "lord-icon" custom element with default properties
+defineElement(lottie.loadAnimation);
+const Header = () => {
   return (
-    <View style={styles.container}>
-      <Player 
-        ref={playerRef} 
-        style={{ width: 96, height: 96 }} 
-        icon={ ICON }
-      />
-    </View>
-  );
-}
+<eiv>
+<lord-icon
+        src="../Images/Lordicon/wired-flat-146-trolley.json"
+        trigger="hover"
+      ></lord-icon>
+</eiv>
+  )};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    gap: 20,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-    flexDirection: 'column',
-  },
-});
+  export default  Header;
