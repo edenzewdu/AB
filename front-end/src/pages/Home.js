5ca4ae83e8182fd7d06ef04e4a70798a9a604ca7
eden zewdu <edenzewdu434@gmail.com>
2023-10-23 05:15:11 +0300

    creating animation

--- front-end/src/pages/Home.js
+++ front-end/src/pages/Home.js
@@ -1,18 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Home.css';
 import Header from './Header.js';
 
 const NetelaAnimation = () => {
-  const animateNetela = () => {
-    const netela = document.getElementById("netela");
-    netela.style.top = "0px";
-    netela.style.left = "0px";
-    netela.style.opacity = "0";
+  const [isAnimating, setIsAnimating] = useState(false);
 
-    setTimeout(function() {
-      netela.style.display = "none";
-      document.getElementById("wardrobe").innerHTML +=
-        '<div style="position: absolute; top: 200px; left: 200px; width: 100px; height: 100px; background-image: url(\'netela_image.png\'); background-size: cover;"></div>';
+  const animateNetela = () => {
+    setIsAnimating(true);
+    setTimeout(() => {
+      setIsAnimating(false);
     }, 1000); // adjust the timeout value to match the transition duration
   };
 
@@ -21,16 +17,30 @@ const NetelaAnimation = () => {
       <button onClick={animateNetela}>Start Animation</button>
 
       <div id="person"></div>
-      <div id="wardrobe"></div>
+      <div id="wardrobe">
+        {!isAnimating && (
+          <div
+            style={{
+              position: 'absolute',
+              top: '200px',
+              left: '200px',
+              width: '100px',
+              height: '100px',
+              backgroundImage: `url('../Images/netelaAnimation.jpg')`,
+              backgroundSize: 'cover',
+            }}
+          ></div>
+        )}
+      </div>
     </>
   );
 };
 
 const Home = () => {
   return (
-    <div className='home'>
+    <div className="home">
       <Header />
-      <div className='body'>
+      <div className="body">
         <h1>Welcome to Ethiopian Traditions</h1>
         <p>Discover the beauty of traditional Ethiopian clothing</p>
         <button className="shop-now-btn">Shop Now</button>
