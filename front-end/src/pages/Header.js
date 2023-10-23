b6d80d66424a2ff343f3d8d251fd3407b4fb9835
eden zewdu <edenzewdu434@gmail.com>
2023-10-23 04:55:24 +0300

    creating animation in the body

--- front-end/src/pages/Header.js
+++ front-end/src/pages/Header.js
@@ -8,15 +8,55 @@ defineElement(lottie.loadAnimation);
 const Header = () => {
   return (
     <div>
-      <div>
-<lord-icon
-    src="https://cdn.lordicon.com/cosvjkbu.json"
-    trigger="hover"
-    colors="primary:#6c16c7,secondary:#4f1091,tertiary:#320a5c"
-    style={{width:'100px',height:'100px'}}>
-</lord-icon>
+      <div className='header'>
+        <div className='gift'>
+          <lord-icon
+            src="https://cdn.lordicon.com/pgmktfgp.json"
+            trigger="hover"
+            colors="primary:#c69cf4,secondary:#320a5c"
+            className='gift-icon'
+           ></lord-icon>
+        </div>
+        <div className='cart'>
+          <lord-icon
+            src="https://cdn.lordicon.com/cosvjkbu.json"
+            trigger="hover"
+            colors="primary:#6c16c7,secondary:#4f1091,tertiary:#320a5c"
+            className='cart-icon'
+          ></lord-icon>
+        </div>
       </div>
-      <div>
+      <div className='pp' >
+            
+              <div className='ppp' >
+                <div className='profile-picture' >
+                   <img alt="Profile" className="logo-image" style={{
+                      width: '50px',
+                      height: '50px',
+                      borderRadius: '50%',
+                      marginRight: '10px',
+                    }} />
+                    <div style={{ width: '45px', height: '45px', margin: '17px', marginRight: '10px', borderRadius: '50%', backgroundImage: 'linear-gradient(to right, rgb(250, 130, 10), rgb(300, 200, 0))', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+                      <span style={{ fontSize: '24px', color: 'white', justifyContent: 'center' }}>
+                        </span>
+                    </div>
+                  <div style={{ fontSize: '17px', fontStyle: 'italic', fontWeight: 'bold', justifyContent: 'center', marginTop: '30px', marginRight: '10px' }}></div>
+                </div>
+                <div className='login-box' style={{ justifyContent: 'right' }}>
+                  <a className='login' >
+                    Logout
+                  </a>
+                </div>
+              </div>
+              <div className='login-section'>
+                <div className='login-box' style={{
+                  textAlign: 'right', width: '110PX'
+                }}>
+                  <img src={{}} alt='login-icon' className='login-icon' style={{ width: '20PX' }}></img>
+                </div>
+              </div>
+          </div>
+          <div className='logo'>
         <img src={require('../Images/AB_ETHIOPIAN_TRADITIONAL_CLOTHES__2_-removebg-preview.png')} alt='logo' />
       </div>
     </div>
--- front-end/src/pages/Home.js
+++ front-end/src/pages/Home.js
@@ -4,11 +4,13 @@ import Header from './Header.js';
 
 const Home = () => {
   return (
-    <div>
+    <div className='home'>
       <Header />
+      <div className='body'>
       <h1>Welcome to Ethiopian Traditions</h1>
       <p>Discover the beauty of traditional Ethiopian clothing</p>
       <button className="shop-now-btn">Shop Now</button>
+    </div>
     </div>
   );
 };
