2dd0496ec4d84abcd8efbdac2abe075e14c25c18
eden zewdu <edenzewdu434@gmail.com>
2023-10-24 23:22:17 +0300

    creating animation

--- front-end/src/pages/Home.js
+++ front-end/src/pages/Home.js
@@ -7,7 +7,7 @@ import wardrobeImage from '../Images/wardrobe.png';
 
 const NetelaAnimation = () => {
   const [scrollPosition, setScrollPosition] = useState(0);
-  const [netellaPosition, setNetellaPosition] = useState(500);
+  const [netelaVisible, setNetelaVisible] = useState(false);
 
   useEffect(() => {
     const handleScroll = () => {
@@ -23,33 +23,36 @@ const NetelaAnimation = () => {
   }, []);
 
   useEffect(() => {
-    if (scrollPosition < 50) {
-      setNetellaPosition(50);
-    } else if (scrollPosition >= 50 && scrollPosition < 100) {
-      setNetellaPosition(scrollPosition);
+    const windowHeight = window.innerHeight;
+    const netelaAnimationImageHeight = 1000; // Adjust this value based on the actual height of the netelaAnimationImage
+
+    if (scrollPosition >= windowHeight && scrollPosition <= windowHeight + netelaAnimationImageHeight) {
+      setNetelaVisible(true);
     } else {
-      setNetellaPosition(1000);
+      setNetelaVisible(false);
     }
   }, [scrollPosition]);
 
   return (
     <div className="App" style={{ height: '2000px' }}>
       <div className="container" style={{ position: 'relative' }}>
-        <img
-          src={netelaAnimationImage}
-          alt="Beautiful Girl with Netella"
-          style={{
-            position: 'absolute',
-            top: `${scrollPosition}px`,
-            transition: 'top 0.5s ease',
-          }}
-        />
+        {netelaVisible && (
+          <img
+            src={netelaAnimationImage}
+            alt="Beautiful Girl with Netella"
+            style={{
+              position: 'absolute',
+              top: `${scrollPosition - window.innerHeight}px`,
+              transition: 'top 0.5s ease',
+            }}
+          />
+        )}
         <img
           src={netellaImage}
           alt="Netella"
           style={{
             position: 'absolute',
-            top: `${netellaPosition}px`,
+            top: `${scrollPosition}px`,
             transition: 'top 0.5s ease',
           }}
         />
