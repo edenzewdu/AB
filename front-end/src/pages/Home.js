735b6f44c9e19fffe94a2b9615d19a095a157715
eden zewdu <edenzewdu434@gmail.com>
2023-10-24 23:52:48 +0300

    creating home page with animations

--- front-end/src/pages/Home.js
+++ front-end/src/pages/Home.js
@@ -5,80 +5,84 @@ import netelaAnimationImage from '../Images/netelaAnimation.jpg';
 import netellaImage from '../Images/netelaAnimation-removebg-preview.png';
 import wardrobeImage from '../Images/wardrobe.png';
 
-const NetelaAnimation = () => {
-  const [scrollPosition, setScrollPosition] = useState(0);
-  const [netelaVisible, setNetelaVisible] = useState(false);
+// const NetelaAnimation = () => {
+//   const [scrollPosition, setScrollPosition] = useState(0);
+//   const [netelaVisible, setNetelaVisible] = useState(false);
 
-  useEffect(() => {
-    const handleScroll = () => {
-      const position = window.scrollY;
-      setScrollPosition(position);
-    };
+//   useEffect(() => {
+//     const handleScroll = () => {
+//       const position = window.scrollY;
+//       setScrollPosition(position);
+//     };
 
-    window.addEventListener('scroll', handleScroll);
+//     window.addEventListener('scroll', handleScroll);
 
-    return () => {
-      window.removeEventListener('scroll', handleScroll);
-    };
-  }, []);
+//     return () => {
+//       window.removeEventListener('scroll', handleScroll);
+//     };
+//   }, []);
 
-  useEffect(() => {
-    const windowHeight = window.innerHeight;
-    const netelaAnimationImageHeight = 1000; // Adjust this value based on the actual height of the netelaAnimationImage
+//   useEffect(() => {
+//     const windowHeight = window.innerHeight;
+//     const netelaAnimationImageHeight = 1000; // Adjust this value based on the actual height of the netelaAnimationImage
 
-    if (scrollPosition >= windowHeight && scrollPosition <= windowHeight + netelaAnimationImageHeight) {
-      setNetelaVisible(true);
-    } else {
-      setNetelaVisible(false);
-    }
-  }, [scrollPosition]);
+//     if (scrollPosition >= windowHeight && scrollPosition <= windowHeight + netelaAnimationImageHeight) {
+//       setNetelaVisible(true);
+//     } else {
+//       setNetelaVisible(false);
+//     }
+//   }, [scrollPosition]);
 
-  return (
-    <div className="App" style={{ height: '2000px' }}>
-      <div className="container" style={{ position: 'relative' }}>
-        {netelaVisible && (
-          <img
-            src={netelaAnimationImage}
-            alt="Beautiful Girl with Netella"
-            style={{
-              position: 'absolute',
-              top: `${scrollPosition - window.innerHeight}px`,
-              transition: 'top 0.5s ease',
-            }}
-          />
-        )}
-        <img
-          src={netellaImage}
-          alt="Netella"
-          style={{
-            position: 'absolute',
-            top: `${scrollPosition}px`,
-            transition: 'top 0.5s ease',
-          }}
-        />
-        <img
-          src={wardrobeImage}
-          alt="Wardrobe"
-          style={{
-            position: 'absolute',
-            top: `${scrollPosition + 1000}px`,
-            transition: 'top 0.5s ease',
-          }}
-        />
-      </div>
-    </div>
-  );
-};
+//   return (
+//     <div className="App" style={{ height: '2000px' }}>
+//       <div className="container" style={{ position: 'relative' }}>
+//         {netelaVisible && (
+//           <img
+//             src={netelaAnimationImage}
+//             alt="Beautiful Girl with Netella"
+//             style={{
+//               position: 'absolute',
+//               top: `${scrollPosition - window.innerHeight}px`,
+//               transition: 'top 0.5s ease',
+//             }}
+//           />
+//         )}
+//         <img
+//           src={netellaImage}
+//           alt="Netella"
+//           style={{
+//             position: 'absolute',
+//             top: `${scrollPosition}px`,
+//             transition: 'top 0.5s ease',
+//           }}
+//         />
+//         <img
+//           src={wardrobeImage}
+//           alt="Wardrobe"
+//           style={{
+//             position: 'absolute',
+//             top: `${scrollPosition + 1000}px`,
+//             transition: 'top 0.5s ease',
+//           }}
+//         />
+//       </div>
+//     </div>
+//   );
+// };
 
 const Home = () => {
   return (
     <div className="home">
       <Header />
       <div className="body">
-        <h1>Welcome to Ethiopian Traditions</h1>
-        <p>Discover the beauty of traditional Ethiopian clothing</p>
-        <button className="shop-now-btn">Shop Now</button>
-        <NetelaAnimation />
+        <div>
+          <video src="../Images/beauty girl wind blows.mp4" controls width="640" height="360" />
+        </div>
+        <div>
+          <h1>Welcome to Ethiopian Traditions</h1>
+          <p>Discover the beauty of traditional Ethiopian clothing</p>
+          <button className="shop-now-btn">Shop Now</button>
+        </div>
       </div>
     </div>
   );
