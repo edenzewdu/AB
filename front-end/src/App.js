2759032eec215c646605883b59f022845084d416
eden zewdu <edenzewdu434@gmail.com>
2023-10-26 11:19:13 +0300

    install react-icons

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import AboutPage from './pages/About';
 import './App.css';
@@ -8,10 +8,10 @@ function App() {
   return (
     <div className="App">
       <Router>
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route path="/about" component={AboutPage} />
-        </Switch>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<AboutPage />} />
+        </Routes>
       </Router>
     </div>
   );
