7fa2e9233f1cd030b4160ea28e3007bfa44c9607
eden zewdu <edenzewdu434@gmail.com>
2023-10-29 15:55:36 +0300

    creating admin login page

---
+++ front-end/src/pages/Admin/AdminLogin.js
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
+
+function AdminLogin() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const history = useHistory();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Perform login authentication here using an API or any other method
+    // If login is successful, navigate to the dashboard
+    history.push('/dashboard');
+  };
+
+  return (
+    <div>
+      <h2>Login Page</h2>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+}
+
+export default AdminLogin;
